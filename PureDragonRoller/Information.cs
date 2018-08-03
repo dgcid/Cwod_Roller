bd337044e50b89dabc9cd3ba1f7146d0036822e6
Andrew Rice <dgcid@users.noreply.github.com>
2018-08-03 12:35:13 -0500

    Added Info to puredragon

---
+++ PureDragonRoller/Information.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WraithRoller
+{
+    public class Information
+    {
+     
+
+        //Wraith Fields
+        public int Wrastr { get; set; }
+        public int Wradex { get; set; }
+        public int Wrastam { get; set; }
+        public int Wrachar { get; set; }
+        public int wraman { get; set; }
+        public int wraapp { get; set; }
+        public int wraperc { get; set; }
+        public int wraintel { get; set; }
+        public int wrawits { get; set; }
+
+        public int wraalert { get; set; }
+        public int wraath { get; set; }
+        public int wraawa { get; set; }
+        public int wradod { get; set; }
+        public int wrabra { get; set; }
+        public int wraemp { get; set; }
+        public int wraexp { get; set; }
+        public int wrainti { get; set; }
+        public int wrastreet { get; set; }
+        public int wrasubt { get; set; }
+        public int wracraft { get; set; }
+        public int wradrive { get; set; }
+        public int wraeti { get; set; }
+        public int wrafa { get; set; }
+        public int wralead { get; set; }
+        public int wramedit { get; set; }
+        public int wramel { get; set; }
+        public int wraperf { get; set; }
+        public int wrasec { get; set; }
+        public int wrasteal { get; set; }
+        public int wraacad { get; set; }
+        public int wracomp { get; set; }
+        public int wraenig { get; set; }
+        public int wrainvest { get; set; }
+        public int wralaw { get; set; }
+        public int wraling { get; set; }
+        public int wramed { get; set; }
+        public int wraocc { get; set; }
+        public int wrapoli { get; set; }
+        public int wrasci { get; set; }
+        public int corpus { get; set; }
+        public int wraWill { get; set; }
+        public int wraMaxWill { get; set; }
+        public int wracurcorp { get; set; }
+        public string wraName { get; set; }
+        public String wraLife { get; set; }
+        public String wraDeath { get; set; }
+        public int wrapathos { get; set; }
+        public int wracurpathos { get; set; }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
